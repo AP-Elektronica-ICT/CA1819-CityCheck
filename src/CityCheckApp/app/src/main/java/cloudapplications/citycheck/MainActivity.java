6986b8d28460d4f608fe96b9bcaa6109cbb511e2
Mirko Messina <mirko.messina@student.ap.be>
2018-10-15 16:09:55 +0200

    UI elementen aan de app toegevoegd

--- src/CityCheckApp/app/src/main/java/cloudapplications/citycheck/MainActivity.java
+++ src/CityCheckApp/app/src/main/java/cloudapplications/citycheck/MainActivity.java
@@ -2,6 +2,8 @@ package cloudapplications.citycheck;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button createGame = (Button) findViewById(R.id.button_create_game);
+        Button joinGame = (Button) findViewById(R.id.button_join_game);
+
+        createGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        joinGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }

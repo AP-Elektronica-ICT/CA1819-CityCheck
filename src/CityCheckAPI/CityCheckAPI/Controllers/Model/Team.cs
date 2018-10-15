d3459036be50d74f8c0e8b97a4be1149256fb083
Elke <elke.renard@gmail.com>
2018-10-15 13:33:25 +0200

    classes team, locatie, teamtrace

---
+++ src/CityCheckAPI/CityCheckAPI/Controllers/Model/Team.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityCheckAPI.Controllers.Model
+{
+    public class Team
+    {
+        public int IdTeam { get; set; }
+        public int Punten { get; set; }
+        public string Kleur { get; set; }
+        public string TeamNaam { get; set; }
+        public Locatie HuidigeLocatie { get; set;}
+        public List<TeamTrace> TeamTraces { get; set; }
+    }
+}

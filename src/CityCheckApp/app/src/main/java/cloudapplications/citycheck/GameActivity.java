ed0dd829eca871cdaabad5fb287b6fe2fe420c7e
SVEngelgem <stijnvanengelgem@hotmail.com>
2018-10-18 11:23:49 +0200

    voorbereiden merge

--- src/CityCheckApp/app/src/main/java/cloudapplications/citycheck/GameActivity.java
+++ src/CityCheckApp/app/src/main/java/cloudapplications/citycheck/GameActivity.java
@@ -37,10 +37,9 @@ public class GameActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng antwerpen = new LatLng(51.2120125,4.4115053);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(antwerpen,14.0f));
     }
 }

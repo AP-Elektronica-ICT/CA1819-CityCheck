91bd3d6d07a48a53cbefb6b414bee8ab2a359785
Brent Van Tilborg <brent.van.tilborg@telenet.be>
2018-11-08 12:10:38 +0100

    begin colorpick

--- src/CityCheckApp/app/src/main/java/cloudapplications/citycheck/JoinGameActivity.java
+++ src/CityCheckApp/app/src/main/java/cloudapplications/citycheck/JoinGameActivity.java
@@ -2,12 +2,36 @@ package cloudapplications.citycheck;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class JoinGameActivity extends AppCompatActivity {
+
+    private Button btnPickColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_game);
+
+        btnPickColor = (Button) findViewById(R.id.button_pick_color);
+
+        btnPickColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(getApplicationContext(), "kleur kiezen", Toast.LENGTH_LONG).show();
+
+                //Kleurkiezer weergeven
+
+
+
+
+            }
+        });
+
+
+
+
     }
 }

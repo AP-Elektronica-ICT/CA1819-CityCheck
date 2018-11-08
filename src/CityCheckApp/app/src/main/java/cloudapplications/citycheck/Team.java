614519cd88809c27a8cc7346bd7f8062ea410f8e
Elke <elke.renard@gmail.com>
2018-11-08 13:27:26 +0100

    aanpassing structuur teams

--- src/CityCheckApp/app/src/main/java/cloudapplications/citycheck/Team.java
+++ src/CityCheckApp/app/src/main/java/cloudapplications/citycheck/Team.java
@@ -1,61 +1,44 @@
 package cloudapplications.citycheck;
-
-import android.util.Log;
-
 import com.google.android.gms.maps.model.LatLng;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-
-import java.io.IOException;
 import java.util.List;
 
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.Response;
 
 public class Team {
+
     //properties van een team
-    public String Name;
-    public String Colour;
-    public long CurrentLong;
-    public long CurrentLat;
-    public int Id;
-    public int Punten;
-    public List<LatLng> Traces;
+    private String Name;
+    private String Colour;
+    private long CurrentLong;
+    private long CurrentLat;
+    private int Id;
+    private int Punten;
+    private List<TeamTrace> Traces;
 
-    //variabelen om teams op te halen uit database
-    OkHttpCall call = new OkHttpCall();
-    private List<Team> teams;
+    public int getId() {
+        return Id;
+    }
 
-    public List<Team> getTeams(String teamName, int gameId){
+    public int getPunten() {
+        return Punten;
+    }
 
-        Call response = call.get(String.format("http://84.197.102.107/api/citycheck/currentgame/teams/%d", gameId), new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                Log.d("Teams", "getTeams: failed");
-            }
+    public long getCurrentLat() {
+        return CurrentLat;
+    }
 
-            @Override
-            public void onResponse(Call call, Response response) {
-                if (response.isSuccessful()) {
-                    try {
-                        JSONArray array = new JSONArray(response.body().toString());
-                        for(int i=0; i<array.length(); i++){
-                            teams.add((Team)array.get(i));
-                        }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                    Log.d("Teams", "getTeams: " + teams);
-                    // Do what you want to do with the response.
+    public String getColour() {
+        return Colour;
+    }
 
-                } else {
-                    // Request not successful
-                }
-            }
-        });
+    public String getName() {
+        return Name;
+    }
 
-        return teams;
+    public long getCurrentLong() {
+        return CurrentLong;
+    }
+
+    public List<TeamTrace> getTraces() {
+        return Traces;
     }
 }
---
+++ src/CityCheckApp/app/src/main/java/cloudapplications/citycheck/TeamTrace.java
@@ -0,0 +1,19 @@
+package cloudapplications.citycheck;
+
+class TeamTrace {
+    private int Id;
+    private long Long;
+    private long Lat;
+
+    public int getId() {
+        return Id;
+    }
+
+    public long getLat() {
+        return Lat;
+    }
+
+    public long getLong() {
+        return Long;
+    }
+}

7fef2b01a4d9ab80b7c714f769f82ae2e7dee8e9
BrentVanTilborg <brent.van.tilborg@telenet.be>
2018-11-15 22:00:54 +0100

    login ui added

--- src/AdminApp/src/app/app.module.ts
+++ src/AdminApp/src/app/app.module.ts
@@ -1,14 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ButtonsModule, CardsFreeModule, MDBBootstrapModule, WavesModule} from 'angular-bootstrap-md';
+import { RouterModule, Routes } from '@angular/router';
+const appRoutes: Routes = [
+  {
+    path: 'doel',
+    component: DoelComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  { path: '',
+    component: DoelComponent
+  },
+  { path: '**', component: DoelComponent }
+];
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DoelComponent } from './doel/doel.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DoelComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(
+      appRoutes
+      ),
+    MDBBootstrapModule.forRoot(),
+    FormsModule,
+    WavesModule,
+    CardsFreeModule,
+    ButtonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/AdminApp/src/app/doel/doel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-doel',
+  templateUrl: './doel.component.html',
+  styleUrls: ['./doel.component.scss']
+})
+export class DoelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

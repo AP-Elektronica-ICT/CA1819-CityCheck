72007a67bda6349605faf988e8bdc0b9fc6a9fbb
Brent Van Tilborg <brent.van.tilborg@telenet.be>
2018-12-13 09:24:46 +0100

    added dialog picker voor de tijdsduur

--- src/CityCheckApp/app/src/main/java/cloudapplications/citycheck/CreateGameActivity.java
+++ src/CityCheckApp/app/src/main/java/cloudapplications/citycheck/CreateGameActivity.java
@@ -1,5 +1,6 @@
 package cloudapplications.citycheck;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -17,6 +19,7 @@ import org.json.JSONObject;
 public class CreateGameActivity extends AppCompatActivity {
 
     private int gameTime;
+    private Button btnTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,16 @@ public class CreateGameActivity extends AppCompatActivity {
 
         Button createGameButton = findViewById(R.id.button_create_game);
         Spinner gameTimeSpinner = findViewById(R.id.spinner_game_time);
+
+        btnTime = (Button) findViewById(R.id.button_timePick);
+
+        btnTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //We gaan een tijd selecteren
+                timePick();
+            }
+        });
 
         String[] items = new String[]{"1", "2", "3", "10 seconds"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
@@ -72,4 +85,49 @@ public class CreateGameActivity extends AppCompatActivity {
             Toast.makeText(this, "Error while trying to create a new game", Toast.LENGTH_SHORT).show();
         }
     }
+
+
+
+        public void timePick(){
+
+            final Dialog d = new Dialog(CreateGameActivity.this);
+            d.setTitle("NumberPicker");
+            d.setContentView(R.layout.timepick_dialog);
+            Button b1 = (Button) d.findViewById(R.id.btn_timeSet);
+            Button b2 = (Button) d.findViewById(R.id.btn_timeCancel);
+            final NumberPicker np = (NumberPicker) d.findViewById(R.id.numberPicker);
+            np.setMaxValue(4);
+            np.setMinValue(1);
+            np.setWrapSelectorWheel(false);
+            np.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+                @Override
+                public void onValueChange(NumberPicker numberPicker, int old, int newTijd) {
+                    Toast.makeText(getApplicationContext(), "Time: "+Integer.toString(newTijd), Toast.LENGTH_SHORT).show();
+                };
+            });
+            b1.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v) {
+                    //de gekozen tijd ophalen
+                    gameTime = np.getValue();
+                    Toast.makeText(getApplicationContext(), "Time: "+Integer.toString(gameTime), Toast.LENGTH_LONG).show();
+                    //Dialog sluiten
+                    d.dismiss();
+                }
+            });
+            b2.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v) {
+                    //Annuleren
+                    d.dismiss();
+                }
+            });
+            d.show();
+
+
+    }
+
+
 }
